76ab14733327d91fa9101246dd47ac2f6c05c2c4
mikecoolio <mikepoon604@gmail.com>
2022-08-04 11:04:43 -0700

    made lessons index view page and basic lessons controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :edit, :edit_password, :update]
+  resources :users, only: [:new, :create, :edit, :edit_password, :update] do
+    resources :lessons, only: [:new, :create, :destroy, :edit, :update, :index]
+  end
   root "welcome#new"
 end
