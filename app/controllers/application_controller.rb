e6a4be7766bc01f18da1ecf0e37e858d19c00149
mikecoolio <mikepoon604@gmail.com>
2022-08-04 11:14:48 -0700

    fixed typos and added to welcome new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   
     
     def current_user
-        @current_user ||= User.find_by_id(session[:value])
+        @current_user ||= User.find_by_id(session[:user_id])
     end
     helper_method :current_user
 
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
         current_user.present?
     end
     helper_method :user_sign_in?
+    
     def authenticate_user!
         redirect_to new_session_path,  notice: "Please sign up" unless user_sign_in?
     end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+    def new
+        @user = current_user
+    end
 end
