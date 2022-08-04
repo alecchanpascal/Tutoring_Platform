8cfc29e7c0c7767b5c60570accd2be3a00090749
loveyourcode2021 <dlee513@my.bcit.ca>
2022-08-04 14:29:18 -0700

    created enrollment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,16 @@ class ApplicationController < ActionController::Base
         current_user.present?
     end
     helper_method :user_sign_in?
-    
+    # if it is a tutor, you not allowed to register course
+    def is_tutor?
+        redirect_to root_path,  notice: "Tutor can not register a course" if current_user.is_tutor == true
+    end
+
+    # if it is a tutor, you not allowed to register course
+    def is_student?
+        redirect_to new_session_path,  notice: "A student can not register a course" if current_user.is_tutor == false
+    end
+
     def authenticate_user!
         redirect_to new_session_path,  notice: "Please sign up" unless user_sign_in?
     end
