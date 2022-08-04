03fc2d663064a9cccab713e874a14be70fde662b
mikecoolio <mikepoon604@gmail.com>
2022-08-04 10:16:04 -0700

    finished basic ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,27 @@
-alias_action :create, :read, :update, :delete, to: :crud
+class Ability
+    include CanCan::Ability
 
-can :crud, Lesson do |lesson|
-    user == Lesson.user
+    def initialize(user)
+        user ||= User.new
+    end
+
+    alias_action :create, :read, :update, :delete, to: :crud
+
+    can :crud, Lesson do |lesson|
+        user == Lesson.user
+    end
+    
+    can :crud, Review do |review|
+        user == persisted? && review.user != user
+    end
+    
+    # can :review, Review do |review|
+    #     student.p
+    # end
+    
+    can :destroy, :Review do |review|
+        user == Review.user
+    end
 end
 
-can :crud, Review do |review|
-    user == Review.user
-end
-
-can :destroy, :Review do |review|
-    user == Review.user
-end
+  end
