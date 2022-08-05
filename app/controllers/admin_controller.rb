5776805bede74f4c97fd4decfb2cd743553b3402
mikecoolio <mikepoon604@gmail.com>
2022-08-04 19:00:24 -0700

    made delete button for lesson show page within the admin tutor panel, fixed wrong end keyword placement in ability.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,10 @@
 class AdminController < ApplicationController
-
+  before_action :find_tutor_id, only: [:index]
   def index
-    @lessons = Lesson.all
-    @lesson = Lesson.find_by_id(params[:tutor_id])
+
+    @lessons = Lesson.all.order(created_at: :desc)
+    # @lesson = Lesson.find_by_id(params[:tutor_id])
+    
     # @lesson.tutor_id(current_user.id)     
 #     @students = Enrollment.student_id
   end
@@ -15,8 +17,25 @@ class AdminController < ApplicationController
 
   end
 
+  def destroy
+    p "INSIDE ADMIN DESTROY"
+    @lesson = Lesson.find params[:id]
+    if @lesson.present?
+      @lesson.destroy
+      redirect_to lessons_path
+    else
+      render '/admin/index', status: 303 
+      # 303 See Other redirect status response code indicates that the redirects don't link to the requested resource itself, but to another page (such as a confirmation page, a representation of a real-world object
+    end
+  end
+
 #   def show
 #     @lesson = Lesson.tutor_id(current_user.id)     
 #     @students = Enrollment.student_id
 #   end
+  private
+
+  def find_tutor_id
+    @lesson = Lesson.find_by_id(params[:tutor_id])
+  end
 end
